3570af52bf9784ef5120cca16fb669ede23c7e5e
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-10-23 12:07:20 +0100

    Add Swap corruption

--- datashredder/datashredder.py
+++ datashredder/datashredder.py
@@ -8,7 +8,7 @@ import binascii
 import random
 from tqdm import tqdm
 
-VERSION = "0.1.2"
+VERSION = "0.1.3"
 
 def corrupt(input_file,output_file,chance=1000,data=b"00"):
     """### corrupts a file with a chosen corruption data
@@ -72,6 +72,32 @@ def random_data_corrupt(input_file,output_file,chance=1000):
     f = open(output_file,"wb")
     f.write(binascii.unhexlify(file))
 
+def swap_corrupt(input_file,output_file):
+    """corrupts a file with swapping the next piece of data 
+    e.g:
+    31 32 44 35 36 37 38 39 30 --> 32 44 35 36 37 38 39 30 
+
+    This will corrupt the entire file
+    Args:
+        input_file ([str]): [input file]
+        output_file ([str]): [output file]
+    """
+    with open(input_file, 'rb') as f:
+        content = f.read()
+    file_hex = binascii.hexlify(content)
+
+    n = 2
+    split_file_hex = [file_hex[index : index + n] for index in range(0, len(file_hex), n)]
+
+    file = ""
+    corruption_times = 0
+    
+
+    for i, ele in enumerate(split_file_hex[:-1]):
+    
+        file = file+split_file_hex[i + 1].decode()
+    f = open(output_file,"wb")
+    f.write(binascii.unhexlify(file))
 if __name__ == "__main__":
     print("Welcome to Datashredder Demo")
     print("Version:"+VERSION)
