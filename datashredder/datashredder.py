13abc9fa7b389a0627720e8a60f1ec0c35c9946c
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-10-30 09:54:39 +0100

    add language surpport

--- datashredder/datashredder.py
+++ datashredder/datashredder.py
@@ -3,7 +3,7 @@ DataShredder - By awesomelewis2007
 A simple corruption engine made in python
 Github:https://github.com/awesomelewis2007/Datashredder
 """
-
+import configparser
 import binascii
 import random 
 from tqdm import tqdm
@@ -21,40 +21,64 @@ def random_corrupt(input_file,output_file,chance=1000):
 def swap_corrupt(input_file,output_file):
     swp_corrupt.swap_corrupt(input_file,output_file)
 
+
+config = configparser.ConfigParser()
+config.read('settings.ini')
+
+language_config = configparser.ConfigParser()
+language_config.read('languages/language.ini')
+
+language = config['general']['language']
+
+class print_language:
+    welcome= language_config[language]['welcome']
+    version= language_config[language]['version']
+    choice_message_line_1= language_config[language]['choice_message_line_1']
+    choice_message_line_2= language_config[language]['choice_message_line_2']
+    choice_message_line_3= language_config[language]['choice_message_line_3']
+    choice_message_line_4= language_config[language]['choice_message_line_4']
+    choice_input= language_config[language]['choice_input']
+    choice_input_error= language_config[language]['choice_input_error']
+    file_in_input=language_config[language]['file_in_input']
+    file_out_input=language_config[language]['file_out_input']
+    chance_input=language_config[language]['chance_input']
+    data_input=language_config[language]['data_input']
+    done_message=language_config[language]['done_message']
 if __name__ == "__main__":
-    print("Welcome to Datashredder Demo")
-    print("Version:"+VERSION)
-    print("Please chose a corruption method")
-    print("Type 1 to use basic corrupt")
-    print("Type 2 to use random basic corrupt")
-    print("Type 3 to use swap corrupt")
+    print(print_language.welcome)
+    print(print_language.version+VERSION)
+    print("")
+    print(print_language.choice_message_line_1)
+    print(print_language.choice_message_line_2)
+    print(print_language.choice_message_line_3)
+    print(print_language.choice_message_line_4)
     while True:
-        choice = input("Enter a number>")
+        choice = input(print_language.choice_input)
         if choice == "1":
             break
         if choice == "2":
             break
         if choice == "3":
             break
-        print("Enter a choice:1,2 or 3 not",choice)
+        print(print_language.choice_input_error,choice)
 
-    file_in = input("Input File>")
-    file_out = input("Output File>")
+    file_in = input(print_language.file_in_input)
+    file_out = input(print_language.file_out_input)
 
     if choice == "1":
-        chance = input("chance of corruption>")
+        chance = input(print_language.chance_input)
         if chance == "":
             chance == "1000"
         chance = int(chance)
 
     if choice == "2":
-        chance = input("chance of corruption>")
+        chance = input(print_language.chance_input)
         if chance == "":
             chance == "1000"
         chance = int(chance) 
 
     if choice == "1":
-        data = input("corruption data>").encode()
+        data = input(print_language.data_input).encode()
         if data == "":
             data == "00"       
     if choice == "1":
@@ -63,4 +87,4 @@ if __name__ == "__main__":
         random_corrupt(input_file=file_in,output_file=file_out,chance=chance)
     if choice == "3":
         swap_corrupt(input_file=file_in,output_file=file_out)
-    print("Corrupted go have a look at your result at: "+file_in)
+    print(print_language.done_message+file_in)
