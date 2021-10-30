7df5766798dd531307be2a162729f69c80a5c1f6
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-10-30 09:08:15 +0100

    improve demo

--- datashredder/datashredder.py
+++ datashredder/datashredder.py
@@ -12,7 +12,7 @@ from engines import corrupt as basic_corrupt
 from engines import random_corrupt as rand_corrupt
 from engines import swap_corrupt as swp_corrupt
 
-VERSION = "0.1.4"
+VERSION = "0.1.6"
 
 def corrupt(input_file,output_file,chance=1000,data=b"00"):
     basic_corrupt.corrupt(input_file,output_file,chance,data)
@@ -24,14 +24,43 @@ def swap_corrupt(input_file,output_file):
 if __name__ == "__main__":
     print("Welcome to Datashredder Demo")
     print("Version:"+VERSION)
+    print("Please chose a corruption method")
+    print("Type 1 to use basic corrupt")
+    print("Type 2 to use random basic corrupt")
+    print("Type 3 to use swap corrupt")
+    while True:
+        choice = input("Enter a number>")
+        if choice == "1":
+            break
+        if choice == "2":
+            break
+        if choice == "3":
+            break
+        print("Enter a choice:1,2 or 3 not",choice)
+
     file_in = input("Input File>")
     file_out = input("Output File>")
-    chance = input("chance of corruption>")
-    if chance == "":
-        chance == "1000"
-    chance = int(chance)
-    data = input("corruption data>").encode()
-    if data == "":
-        data == "00"        
-    corrupt(input_file=file_in,output_file=file_out,chance=chance,data=data)
-    print("Done! Go have a look at your result.")
+
+    if choice == "1":
+        chance = input("chance of corruption>")
+        if chance == "":
+            chance == "1000"
+        chance = int(chance)
+
+    if choice == "2":
+        chance = input("chance of corruption>")
+        if chance == "":
+            chance == "1000"
+        chance = int(chance) 
+
+    if choice == "1":
+        data = input("corruption data>").encode()
+        if data == "":
+            data == "00"       
+    if choice == "1":
+        corrupt(input_file=file_in,output_file=file_out,chance=chance,data=data)
+    if choice == "2":
+        random_corrupt(input_file=file_in,output_file=file_out,chance=chance)
+    if choice == "3":
+        swap_corrupt(input_file=file_in,output_file=file_out)
+    print("Corrupted go have a look at your result at: "+file_in)
