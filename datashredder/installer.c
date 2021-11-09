d313fccda303f9aa95eb8b9213ca991cae176525
awesomelewis2007 <awesomelewis2007@gmail.com>
2021-11-09 06:42:39 +0000

    Update installer

--- datashredder/installer.c
+++ datashredder/installer.c
@@ -2,13 +2,18 @@
 // Github:https://github.com/awesomelewis2007/Datashredder
 #include<stdio.h>
 #include<stdlib.h>
-void install(){
+void python3_install(){
     system("python3 -m pip install --upgrade pip"); 
+    system("pip install tqdm");
+}
+void python_install(){
+    system("python -m pip install --upgrade pip"); 
     system("pip install tqdm");
 }
 int main(){
     char chr;
     printf("Press enter to start the install>>");
-    scanf("%c",&chr);    
-    install();
+    scanf("%c",&chr);   
+    python3_install();
+    python_install();
 }
